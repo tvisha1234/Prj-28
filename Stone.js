9a0cfdfa8f34df8ce680bdce5a45474b400d78a2
tvisha1234 <65931904+tvisha1234@users.noreply.github.com>
2020-09-14 15:17:16 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -1,9 +1,9 @@
 class Stone {
     constructor(x,y,width,height) {
       var options = {
-          isStatic: true,
+          isStatic: false,
           restitution:0,
-          friction:1.0,
+          friction:1,
           density:1.2
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
