9264cc97424dbbf23a99902fa8e7a53499055c16
tvisha1234 <65931904+tvisha1234@users.noreply.github.com>
2020-09-14 15:16:20 +0530

    Update Link.js

--- Link.js
+++ Link.js
@@ -15,12 +15,18 @@ class Link{
         this.link.bodyA = null;
     }
 
+    attach(body) {
+        this.link.bodyA = body;
+    }
+
     display(){
         if(this.link.bodyA){
             var pointA = this.link.bodyA.position;
             var pointB = this.pointB;
+            push();
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop();
         }
     }
     
